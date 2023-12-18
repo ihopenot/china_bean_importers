0d64073b940a7bb5a69177e4378966ae747c52d1
Ihopenot <984430652@qq.com>
2023-12-18 11:21:50 +0800

    update example to my flavor

--- config.example.py
+++ config.example.py
@@ -3,55 +3,57 @@
 from china_bean_importers.common import BillDetailMapping as BDM
 
 config = {
-
-    'importers': {
-        'alipay': {
-            "account": "Assets:Alipay",
+    "importers": {
+        "alipay": {
+            "account": "Assets:Alipay:Balance",
             "huabei_account": "Liabilities:Alipay:HuaBei",
             "yuebao_account": "Assets:Alipay:YueBao",
-            "red_packet_income_account": "Income:Alipay:RedPacket",
-            "red_packet_expense_account": "Expenses:Alipay:RedPacket",
+            "red_packet_income_account": "Income:RedPacket",
+            "red_packet_expense_account": "Expenses:RedPacket",
             "category_mapping": {
-                "交通出行": "Expenses:Travel",
-            }
+                "交通出行": "Expenses:Transport:Others",
+                "餐饮美食": "Expenses:Eating",
+                "教育培训": "Expenses:Education:Others",
+                "日用百货": "Expenses:Daily",
+            },
         },
-        'wechat': {
-            "account": "Assets:WeChat",
+        "wechat": {
+            "account": "Assets:WeChat:Balance",
             "lingqiantong_account": "Assets:WeChat:LingQianTong",
-            "red_packet_income_account": "Income:WeChat:RedPacket",
-            "red_packet_expense_account": "Expenses:WeChat:RedPacket",
+            "red_packet_income_account": "Income:RedPacket",
+            "red_packet_expense_account": "Expenses:RedPacket",
             "family_card_expense_account": "Expenses:WeChat:FamilyCard",
-            "group_payment_expense_account": "Expenses:WeChat:Group",
-            "group_payment_income_account": "Income:WeChat:Group",
-            "transfer_expense_account": "Expenses:WeChat:Transfer",
-            "transfer_income_account": "Income:WeChat:Transfer",
+            "group_payment_expense_account": "Expenses:Unknown",
+            "group_payment_income_account": "Income:Unknown",
+            "transfer_expense_account": "Expenses:Unknown",
+            "transfer_income_account": "Income:Unknown",
         },
-        'thu_ecard': {
+        "thu_ecard": {
             "account": "Assets:Card:THU",
         },
-        'card_narration_blacklist': ["支付宝", "财付通", "美团支付"],
+        "card_narration_blacklist": ["支付宝", "财付通", "美团支付"],
     },
-
-    'card_accounts': {
-        'Liabilities:Card': {
-            "BoC": ["1234", "5678"],
-            "CMB": ["1111", "2222"],
+    "card_accounts": {
+        "Assets": {
+            "BoC:Card": ["XXXX"],
+            "CCB:Card": ["XXXX"],
         },
-        'Assets:Card': {
-            "BoC": ["4321", "8765"],
-            "CMB": ["3333", "4444"],
-        }
     },
-
-    'pdf_passwords': ["123456"],
-
+    "pdf_passwords": ["123456"],
     # account matching
-    'unknown_expense_account': 'Expenses:Unknown',
-    'unknown_income_account': 'Income:Unknown',
-
-    'detail_mappings': [
-        BDM(['京东'], [], 'Expenses:JD', [], {'platform': '京东'}),
-        BDM([], ['饿了么'], 'Expenses:Food:Delivery', [], {'platform': '饿了么'}),
-        BDM([], ['万龙运动旅游'], None, ['ski'], {}),
+    "unknown_expense_account": "Expenses:Unknown",
+    "unknown_income_account": "Income:Unknown",
+    "detail_mappings": [
+        BDM(
+            ["Valve"],
+            ["Steam Purchase"],
+            "Expenses:Entertainment",
+            [],
+            {"platform": "Steam"},
+        ),
+        BDM(["京东"], [], "Expenses:JD", [], {"platform": "京东"}),
+        BDM([], ["饿了么"], "Expenses:Eating", [], {"platform": "饿了么"}),
+        BDM([], ["结息"], "Income:Interest", [], {}),
+        BDM([], ["商铺健身房"], "Expenses:Entertainment", [], {}),
     ],
 }
